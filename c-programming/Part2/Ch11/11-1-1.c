4ae2cf9bbec37f82fdf56e2fc849a150776d115d
JoongHyun Kim <joongh@ewhain.net>
2021-08-21 15:33:51 +0900

    Create 11-1-1.c

---
+++ c-programming/Part2/Ch11/11-1-1.c
@@ -0,0 +1,32 @@
+// 길이가 5인 int형 배열
+// 최댓값, 최솟값, 총합
+
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[5];
+    int max, min, sum, i;
+  
+    for(i = 0; i < 5; i++)
+    {
+        printf("입력: ");
+        scanf("%d", &arr[i]);
+    }
+  
+    max = min = sum = arr[0];
+    for(i = 1; i < 5; i++)
+    {
+        sum += arr[i];
+        if(max < arr[i])
+          max = arr[i];
+        if(min > arr[i])
+          min = arr[i];
+    }
+  
+    printf("최댓값: %d \n", max);
+    printf("최솟값: %d \n", min);
+    printf("총합: %d \n", sum);
+  
+    return 0;
+}
